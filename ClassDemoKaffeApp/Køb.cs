fd70fe64c3a024e8e4554e3b45a955ac7c06ff44
Peter Levinsky <pele@zealand.dk>
2023-10-06 11:12:33 +0200

    Nu med Køb

---
+++ ClassDemoKaffeApp/Køb.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoKaffeApp
+{
+    public class Køb
+    {
+        /*
+         * Instans Felter
+         */
+
+        // Ingen attributter
+
+        // MEN 2 associeringer
+        private Kaffe _kaffe;
+        private Kunde _kunde;
+
+        /*
+         * Properties
+         */
+        public Kaffe Kaffe
+        {
+            get { return _kaffe; }
+            set { _kaffe = value; }
+        }
+
+        public Kunde Kunde
+        {
+            get { return _kunde; }
+            set { _kunde = value; }
+        }
+
+
+
+        /*
+         * Constructor
+         */
+        public Køb()
+        {
+            _kaffe = null;
+            _kunde = null;
+
+        }
+
+        public Køb (Kaffe kaffe, Kunde kunde)
+        {
+            _kaffe = kaffe;
+            _kunde = kunde;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Kaffe)}={Kaffe}, {nameof(Kunde)}={Kunde}}}";
+        }
+    }
+}
--- ClassDemoKaffeApp/Program.cs
+++ ClassDemoKaffeApp/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ClassDemoKaffeApp;
+
 Console.WriteLine("Hello, World!");
+
+
+Kaffe kaffe1 = new Kaffe("Kenya", "Arabica", 55, "Mellem");
+Console.WriteLine(kaffe1);
+
+Kunde kunde1 = new Kunde("peter", "33445566");
+Console.WriteLine(kunde1);
+
+Køb køb1 = new Køb(kaffe1, kunde1);
+Console.WriteLine(køb1);
+
+
+kunde1.Navn = "Jakob";
+Console.WriteLine(kunde1);
+Console.WriteLine(køb1);
+
+køb1.Kunde.Navn = "Per";
+Console.WriteLine(kunde1);
